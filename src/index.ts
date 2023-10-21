62d3ff7b465c3e83733cb8c78e8f6c3b935fbe87
Boateng Gideon <hack3rkofi@gmail.com>
2023-10-21 20:28:17 +0000

    Implement bubble sort algorithm

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { Sorter } from './Sorter';
+import { NumberCollection } from './NumbersCollection';
+import { CharacterCollection } from './characterCollection';
+import { LinkedList } from './LinkedList';
+
+const numberCollection = new NumberCollection([10, -5, 0, -12]);
+const sorter = new Sorter(numberCollection);
+sorter.sort();
+console.log(numberCollection.data);
+
+// const characterCollection = new CharacterCollection('Xaayb');
+// const sorterch = new Sorter(characterCollection);
+// sorterch.sort();
+// console.log(characterCollection.data);
+
+// const linkedList = new LinkedList();
+// linkedList.add(-500);
+// linkedList.add(-3);
+// linkedList.add(456);
+// linkedList.add(3);
+// linkedList.add(5);
+
+// console.log(linkedList.print());
+
+// const sortlinkedList = new Sorter(linkedList);
+// sortlinkedList.sort();
+// console.log(linkedList.print());
