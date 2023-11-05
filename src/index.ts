38b82c0c866014be48d5de5562551a9b5d724826
Boateng Gideon <hack3rkofi@gmail.com>
2023-11-05 19:33:31 +0000

    Using Composition and Interface

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
+import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
 import { matchResults } from './MatchResults';
 
-const reader = new MatchReader('football.csv');
-reader.read();
+//Create an object that satisfies the 'DataReader'interface
+const csvFileReader = new CsvFileReader('football.csv');
+
+//Create an instance of MatchReader and pass in something satisfying the 'DataReader' interface.
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
+
 let manUWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === matchResults.HomeWin) {
     manUWins++;
   } else if (match[2] === 'Man United' && match[5] === matchResults.AwayWin) {
